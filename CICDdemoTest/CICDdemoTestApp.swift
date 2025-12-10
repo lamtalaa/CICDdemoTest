a63d7b314e73a029bb66330c866669da3cd82b8c
Yassine Lamtalaa <ysnlmtl3@gmail.com>
2025-12-09 19:17:00 -0500

    Initial Commit

---
+++ CICDdemoTest/CICDdemoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDdemoTestApp.swift
+//  CICDdemoTest
+//
+//  Created by Yassine Lamtalaa on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CICDdemoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
